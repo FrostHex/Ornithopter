800f08a5fd69a9b3ddecfd29e9d89743a11729df
FrostHex <frosts_mage@163.com>
2025-02-20 17:32:33 +0800

    ✨feat: 无线通信和滑块
    滑块连接上位机控制主翼电机的油门信号, 不再使用串口控制
    延迟主要在无线通信部分, 这版为降低延迟把参数设置得比较激进 (loop中的delay, setRetries, setDataRate)

--- Code_Nano/Code_Nano.ino
+++ Code_Nano/Code_Nano.ino
@@ -12,7 +12,10 @@ Servo servo_main; // 主翼电机
 const int servo_main_pin = 9; // 主翼电机对应的引脚
 const int servo_main_max = 199; // 主翼电机转速信号最大值，对应占空比11.57%
 const int servo_main_min = 8; // 主翼电机转速信号最小值，对应占空比3.15%
-int servo_main_val = 0; // 电机转速信号
+int slider_ratio = 0; // 电机转速信号
+int joystick_x = 0; // 摇杆X轴信号
+int joystick_y = 0; // 摇杆Y轴信号
+byte data[3] = {0x00, 0x00, 0x00}; // 接收数据包
 
 
 void setup() 
@@ -23,39 +26,56 @@ void setup()
   radio.begin(); // 初始化radio对象
   radio.openReadingPipe(1, address); // 设置地址，0-5指定打开的管道
   radio.setPALevel(RF24_PA_LOW); // 设置功率放大器级别，RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
-  radio.setDataRate(RF24_250KBPS); // 设置发送速率，RF24_250KBPS, RF24_1MBPS, RF24_2MBPS
+  radio.setDataRate(RF24_2MBPS); // 设置发送速率，RF24_250KBPS, RF24_1MBPS, RF24_2MBPS
   radio.startListening(); // 设置为接收端
 
-  servo_main.attach(servo_main_pin);
-  delay(1);
-  set_throttle(100); // 设置油门最大
-  Serial.println("Please connect the battery to the ESC in 5 seconds");
-  wait(5);
-  set_throttle(0); // 设置油门最小
-  wait(5);
-  Serial.println("ESC is ready");
+  // servo_main.attach(servo_main_pin);
+  // delay(1);
+  // set_throttle(100); // 设置油门最大
+  // Serial.println("Please connect the battery to the ESC in 5 seconds");
+  // wait(5);
+  // set_throttle(0); // 设置油门最小
+  // wait(5);
+  // Serial.println("ESC is ready");
 }
 
 
 void loop() 
 {
-  // 检天线查是否有数据可读
-  char text[32] = "";
-  radio.read(&text, sizeof(text));
-  Serial.println(text);
-  Serial.println("Listening...");
-  delay(500); // 延时500ms
-
-  // 读取串口数据并设置油门
-  if (Serial.available())
+  // 读取天线数据
+  if (radio.available())
   {
-    int ratio = Serial.parseInt();
-    set_throttle(ratio);
-    while (Serial.available() > 0) 
-    {
-      Serial.read();
-    }
+    radio.read(&data, sizeof(data));
+    slider_ratio = data[0];
+    joystick_x = data[1];
+    joystick_y = data[2];
   }
+  // else
+  // {
+  //   Serial.println("Listening...");
+  // }
+
+
+  // 读取串口设置的油门比例
+  // if (Serial.available())
+  // {
+  //   slider_ratio = Serial.parseInt();
+  //   while (Serial.available() > 0) // 清空串口缓冲区
+  //   {
+  //     Serial.read();
+  //   }
+  // }
+
+  // 设置油门和舵机
+  set_throttle(slider_ratio);
+  Serial.print("Slider ratio: ");
+  Serial.println(slider_ratio);
+  // Serial.print("Joystick X: ");
+  // Serial.println(joystick_x);
+  // Serial.print("Joystick Y: ");
+  // Serial.println(joystick_y);
+
+  delay(10); // 延时ms
 }
 
 
@@ -72,14 +92,14 @@ void wait(int s)
 
 /*
  * @brief 设置油门
- * @param ratio 油门比例
+ * @param ratio 油门比例, 0 ~ 255
  */
 void set_throttle(int ratio)
 {
-  Serial.print("Input ratio: ");
-  Serial.println(ratio);
-  servo_main_val = (int)map(ratio, 0, 100, 8, 199); // 将油门比例转换为对应的计数值，ratio的范围0到100，对应的数值范围8到199
-  servo_main.write(servo_main_val);
+  // Serial.print("Input throttle ratio: ");
+  // Serial.println(ratio);
+  ratio = constrain(ratio, 0, 255); // 限制油门比例在0到255之间
+  servo_main.write((int)map(ratio, 0, 255, 8, 199)); // 将油门比例转换为对应的计数值，ratio的范围0到255，对应的数值范围8到199
   // Serial.print("Setting value: ");
-  // Serial.println(servo_main_val);
+  // Serial.println((int)map(ratio, 0, 255, 8, 199));
 }
--- Code_Uno/Code_Uno.ino
+++ Code_Uno/Code_Uno.ino
@@ -4,37 +4,55 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-// 创建RF24对象，CE对应7号引脚, CSN对应8号引脚
-RF24 radio(7,8);
+RF24 radio(7,8); // 创建RF24对象，CE对应7号引脚, CSN对应8号引脚
+const byte address[6] = "00006"; // 创建地址，用于识别发送端和接收端
 
-// 创建一个地址，用于识别发送端和接收端
-const byte address[6] = "00006";
+const int slider_pin = A0; // 滑杆对应的引脚
+const int slider_max = 653; // 滑杆信号最大值
+const int slider_min = 0; // 滑杆信号最小值
+int slider_val = 0; // 滑杆信号
+int slider_ratio = 0; // 滑杆信号比例, 0-255
+int joystick_x = 0; // 摇杆X轴信号
+int joystick_y = 0; // 摇杆Y轴信号
+byte data[3] = {0x00, 0x00, 0x00}; // 发送数据包
 
 void setup() 
 {
-  // 初始化串口
-  Serial.begin(9600);
-  // 初始化radio对象
-  radio.begin();
+  Serial.begin(9600); // 初始化串口
+  Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n==============================");
 
-  // pinMode(10, OUTPUT);
-  // 设置地址
-  radio.openWritingPipe(address);
-  // 设置功率放大器级别
-  radio.setPALevel(RF24_PA_LOW); // RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
-  
-  // 设置发送速率
-  radio.setDataRate(RF24_250KBPS); // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS
-  
-  // 设置为发送端
-  radio.stopListening();
+  radio.begin(); // 初始化radio对象
+  radio.openWritingPipe(address); // 设置地址
+  radio.stopListening(); // 设置为发送端
+  radio.setPALevel(RF24_PA_LOW); // 设置功率放大器级别, RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate(RF24_2MBPS); // 设置发送速率, RF24_250KBPS, RF24_1MBPS, RF24_2MBPS
+  // radio.setAutoAck(false);  // 禁用自动重发
+  radio.setRetries(3, 10);  // 设置最大重发次数和重发间隔(ms)
 }
 
 void loop() 
 {
   // 发送数据，最多32字节
-  const char text[] = "Hello World";
-  Serial.println("Sending...");
-  Serial.println(radio.write(&text, sizeof(text)));
-  delay(1000);
+  // const char text[] = "Hello World!";
+  // Serial.println("Sending...");
+  // Serial.println(radio.write(&text, sizeof(text)));
+  // delay(1000);
+
+  // 读取滑杆数据
+  slider_val = constrain(analogRead(slider_pin), slider_min, slider_max);
+  slider_ratio = (int) map(slider_val, slider_min, slider_max, 0, 255);
+  // Serial.println(slider_ratio);
+  data[0] = slider_ratio;
+
+  // 发送数据包
+  Serial.print("Sending: ");
+  for (int i = 0; i < sizeof(data); i++)
+  {
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\nResult: ");
+  Serial.println(radio.write(&data, sizeof(data)));
+
+  delay(10); // 延时ms
 }
